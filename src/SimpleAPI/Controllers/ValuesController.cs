4a2254fd06c4c970d3f162f880522b166d9b7592
Amos.Tiao <Amos.Tiao@IL47LP516530>
2019-08-29 10:26:05 -0500

    try first release

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Azure", "Pipeline" };
         }
 
         // GET api/values/5
